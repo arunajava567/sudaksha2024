a101707bff0bef6a6725c59d3e06286fbf08901f
arunajava567 <30722352+arunajava567@users.noreply.github.com>
2024-02-13 10:08:32 +0530

    Add files via upload

---
+++ FilterTableApp/src/App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+import FilterTableComponent from './components/filter.table';
+
+function App() {
+
+  return (
+    <div >
+
+     
+     
+      <FilterTableComponent />
+      
+    </div>
+  );
+}
+
+export default App;
